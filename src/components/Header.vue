2845810969869d95be9d4677b09d2307207af26b
Phyu Thet <phyumyeon@gmail.com>
2024-03-08 13:54:26 +0630

    header change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <!-- Large screen layout -->
-    <v-app-bar app dark color="primary" v-if="$vuetify.breakpoint.mdAndUp">
+    <nav>
+    <v-app-bar theme="dark" app flat>
+
       <v-toolbar-title>Asiavista Expeditions</v-toolbar-title>
+
       <v-spacer></v-spacer>
+
       <v-hover open-delay="100" v-for="link in links" :key="link.text">
         <router-link :to="link.route" class="white--text links">
           <v-btn text :class="{ 'on-hover': hover }" rounded>
@@ -16,41 +20,33 @@
           <router-link to="/register">
             <v-btn rounded="xl" variant="outlined" color="blue-lighten-2">Register</v-btn>
           </router-link>
+
+
           <router-link to="/login">
-            <v-btn prepend-icon="mdi-account" class="ml-2" rounded="xl" variant="outlined" color="green-lighten-2">Login</v-btn>
+            <v-btn prepend-icon="mdi-account" class="ml-2" rounded="xl" variant="outlined"
+              color="green-lighten-2">Login</v-btn>
           </router-link>
         </div>
       </template>
+
       <template v-else>
         <div class="d-flex">
           <v-avatar color="green-lighten-1">
             {{ getUserInitials }}
           </v-avatar>
-          <v-btn @click="authStore.handleLogout" prepend-icon="mdi-account" class="ml-2" rounded="xl" variant="outlined" color="green-lighten-2">LogOut</v-btn>
+
+          <v-btn @click="authStore.handleLogout" prepend-icon="mdi-account" class="ml-2" rounded="xl" variant="outlined"
+            color="green-lighten-2">LogOut</v-btn>
         </div>
+
+
       </template>
+
     </v-app-bar>
+    </nav>
 
     <!-- Small screen layout -->
-    <v-app-bar app dark color="primary" v-else hide-on-scroll>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Asiavista Expeditions</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item v-for="link in links" :key="link.text" link :to="link.route">
-          <v-list-item-title>{{ link.text }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-      <template v-if="!authStore.user">
-        <v-btn @click="goTo('/register')" block>Register</v-btn>
-        <v-btn @click="goTo('/login')" block>Login</v-btn>
-      </template>
-      <template v-else>
-        <v-btn @click="authStore.handleLogout" block>Logout</v-btn>
-      </template>
-    </v-navigation-drawer>
+   
   </div>
 </template>
 
